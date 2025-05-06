8675e01106974ffb7e9e03a9f41b480d3cbec9ad
AdamS <surzyn.adam@gmail.com>
2025-05-06 15:58:40 +0200

    Dev: adding pandas saving to csv before adding a DB connection

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,5 +1,5 @@
 from fetcher import get_poe_data
-
+import pandas as pd
 BASE_URL = 'https://poe.ninja/api/data/currencyoverview'
 SETTLERS_PARAMS = {
     'league': 'Settlers',
@@ -7,6 +7,8 @@ SETTLERS_PARAMS = {
 }
 
 data = get_poe_data(BASE_URL, SETTLERS_PARAMS)
-
 if data:
+    results = data["lines"]
     print(data["lines"][0])
+    poe_df = pd.DataFrame(results)
+    poe_df.to_csv("data_temp/settlers_currency_data.csv", index=False)
