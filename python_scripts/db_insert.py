f3de160cb2dd68d792870cabacad56bb7556ee95
AdamS <surzyn.adam@gmail.com>
2025-05-08 19:00:03 +0200

    Chore: testing connection

---
+++ python_scripts/db_insert.py
@@ -0,0 +1,47 @@
+import psycopg2
+from collections import namedtuple
+from datetime import date
+
+json_data = {
+  "currencyTypeName": "Mirror of Kalandra",
+  "receive": {
+    "sample_time_utc": "2025-05-08T10:34:41.9799164Z",
+    "count": 28,
+    "value": 153054.66564
+  },
+  "detailsId": "mirror-of-kalandra"
+}
+
+def db_insert_currency(currency_data):
+    
+    connection = psycopg2.connect(
+        dbname="poe_currency",
+        user="adam",
+        host="localhost",
+        port="5432"
+        )
+    cursor = connection.cursor()
+    cursor.execute("""
+    INSERT INTO currency_poe (
+        currency_type_name, 
+        sample_time_utc, 
+        count, 
+        value_chaos, 
+        detailsId
+        )
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (currency_type_name, sample_time_utc)
+    DO UPDATE SET
+        count = EXCLUDED.count,
+        value_chaos = EXCLUDED.value_chaos,
+        detailsId = EXCLUDED.detailsId;
+    """, (
+    currency_data.currencyTypeName,
+    currency_data.receive.sample_time_utc,
+    currency_data.receive.count,
+    currency_data.receive.value,
+    currency_data.detailsId
+    ))
+    connection.commit()
+
+db_insert_currency(json_data)
