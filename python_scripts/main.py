37eeecc4656e6bafd1e5cc7301f258857b6f7128
AdamS <surzyn.adam@gmail.com>
2025-05-10 21:11:31 +0200

    Dev: adjusting main to batch insert

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,5 +1,8 @@
 from fetcher import get_poe_data
+from db_insert import db_insert_currency
+from utilities import reformat_all_data
 import pandas as pd
+
 BASE_URL = 'https://poe.ninja/api/data/currencyoverview'
 SETTLERS_PARAMS = {
     'league': 'Settlers',
@@ -9,6 +12,9 @@ SETTLERS_PARAMS = {
 data = get_poe_data(BASE_URL, SETTLERS_PARAMS)
 if data:
     results = data["lines"]
-    print(data["lines"][0])
     poe_df = pd.DataFrame(results)
     poe_df.to_csv("data_temp/settlers_currency_data.csv", index=False)
+    results_reform = reformat_all_data(results)
+    print(type(results_reform))
+    print(results_reform[0])
+    db_insert_currency(results_reform)
