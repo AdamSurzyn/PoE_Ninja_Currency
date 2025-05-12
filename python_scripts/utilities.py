fdf849b5b7d22dcb2651822fa42721a5c045bd69
AdamS <surzyn.adam@gmail.com>
2025-05-12 17:56:36 +0200

    Chore: cleaning format_sample_time

--- python_scripts/utilities.py
+++ python_scripts/utilities.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 import re
-import json
 import pandas as pd
 
 def format_sample_time(time_str):
     sample_time_iso = re.sub(r"\.\d+Z", "", time_str)
     sample_time_utc = datetime.fromisoformat(sample_time_iso)
-    sample_time_hour = sample_time_utc.replace(minute=0, second=0, microsecond=0)
-    return sample_time_hour
+    return sample_time_utc.replace(minute=0, second=0, microsecond=0)
 
 def reformat_dic(currency_dic):
     return (
