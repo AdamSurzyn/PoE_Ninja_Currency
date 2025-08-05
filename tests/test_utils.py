a998686a873597e5ee6c920426ea5ae284e1ce11
AdamS <surzyn.adam@gmail.com>
2025-08-05 20:25:27 +0200

    Chore: removing reduntant files

--- tests/test_utils.py
+++
@@ -1,4 +0,0 @@
-from utils import reformat_currency
-
-def test_utils():
-    assert reformat_currency("Chaos Orb", 12.341) == "Chaos Orb: 12.34c"
--- tests/utils.py
+++
@@ -1,5 +0,0 @@
-# utils.py
-def reformat_currency(name, value):
-    return f"{name}: {round(value, 2)}c"
-
-
