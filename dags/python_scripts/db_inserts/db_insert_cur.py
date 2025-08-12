709c7fa646451d286aa813817a22bfe08f5dd1f2
AdamS <surzyn.adam@gmail.com>
2025-08-12 16:42:37 +0200

    Dev: Slight changes to inserts

--- dags/python_scripts/db_inserts/db_insert_cur.py
+++ dags/python_scripts/db_inserts/db_insert_cur.py
@@ -11,11 +11,30 @@ def db_insert_currency_data():
         currencyTable = Table("currency_rates", metaData, autoload_with=engine)
         stagingTable = Table("currency_rates_stg_raw", metaData, autoload_with=engine)
 
-        stg_select = select(stagingTable.c.currency_type_name, stagingTable.c.sample_time_utc, stagingTable.c.count, stagingTable.c.value_chaos)
+        stg_select = select(
+            stagingTable.c.currency_type_name, 
+            stagingTable.c.sample_time_utc, 
+            stagingTable.c.count, 
+            stagingTable.c.value_chaos
+        )
 
-        stmt = insert(currencyTable).from_select([currencyTable.c.currency_type_name, currencyTable.c.sample_time_utc, currencyTable.c.count, currencyTable.c.value_chaos], stg_select)
+        stmt = insert(currencyTable).from_select(
+            [
+                currencyTable.c.currency_type_name, 
+                currencyTable.c.sample_time_utc, 
+                currencyTable.c.count, 
+                currencyTable.c.value_chaos
+            ], 
+            stg_select
+        )
 
-        stmt = stmt.on_conflict_do_update(index_elements=[currencyTable.c.currency_type_name, currencyTable.c.sample_time_utc], set_={"count": stmt.excluded.count, "value_chaos": stmt.excluded.value_chaos})
+        stmt = stmt.on_conflict_do_update(
+            index_elements=[currencyTable.c.currency_type_name, currencyTable.c.sample_time_utc], 
+            set_={
+                "count": stmt.excluded.count, 
+                "value_chaos": stmt.excluded.value_chaos
+                }
+        )
 
         with engine.begin() as conn: conn.execute(stmt)
 
--- dags/python_scripts/db_inserts/db_insert_stg.py
+++ dags/python_scripts/db_inserts/db_insert_stg.py
@@ -3,8 +3,16 @@ from sqlalchemy.dialects.postgresql import insert
 from python_scripts.utilities import get_sqlalchemy_engine
 import logging
 
+REQUIRED_STG = {
+    "currency_type_name", "detailsid", "sample_time_utc",
+    "count", "value_chaos", "source", "league"
+}
+
 def db_insert_currency(currency_data):
     try:
+        if not currency_data:
+            logging.info("staging: nothing to insert"); 
+            return  
         engine = get_sqlalchemy_engine()
         metaData = MetaData()
         stagingTable = Table("currency_rates_stg_raw", metaData, autoload_with=engine)
