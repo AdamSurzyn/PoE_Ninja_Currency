dd40707fabe334727fcaf58cf46ed56292ee69b5
AdamS <surzyn.adam@gmail.com>
2025-10-26 19:39:15 +0100

    Chore: adding todos

---
+++ src/db_inserts/historical.py
@@ -0,0 +1,8 @@
+#Get item id's from ninja api
+#Iterate through the id's
+#It says how many days ago It needs to add date in this form 2025-10-12 17:00:00 UTC
+#And get data before 15.07 to the beggining of the league.
+#Run as normal insert with merges.
+#Save as tool to get more leagues in dataset.
+#Add items receive - get_id to the currencies dimensions table and staging.
+
