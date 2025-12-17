2b396d441b4c07346c3ece089fb97be87bad5e01
AdamS <surzyn.adam@gmail.com>
2025-12-17 16:28:31 +0100

    Dev: Small fixes to query

--- src/sql/merge_currency_fact.sql
+++ src/sql/merge_currency_fact.sql
@@ -6,18 +6,18 @@ MERGE `${PROJECT}.${DATASET}.currency_rates` AS T USING (
         S.currency_type_name,
         DATE_DIFF(
             DATE(S.sample_time_utc),
-            DATE(L.league_start_timestamp),
+            DATE(L.league_start),
             DAY
         ) AS days_since_league_start
     FROM `${PROJECT}.${DATASET}.currency_rates_stg` S
         JOIN `${PROJECT}.${DATASET}.currency_leagues_dim` L ON S.league = L.league
     WHERE S.sample_time_utc >= @since
-        AND L.league_start_timestamp IS NOT NULL
-        AND DATE(S.sample_time_utc) >= DATE(L.league_start_timestamp)
+        AND L.league_start IS NOT NULL
+        AND DATE(S.sample_time_utc) >= DATE(L.league_start)
     GROUP BY S.league,
         S.sample_time_utc,
         S.currency_type_name,
-        L.league_start_timestamp
+        L.league_start
 ) AS S ON T.currency_type_name = S.currency_type_name
 AND T.sample_time_utc = S.sample_time_utc
 AND T.league = S.league
